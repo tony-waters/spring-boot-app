e1ed6af2808856ed8e7bae75b4c126f88b072570
tony-waters <2tonywaters@gmail.com>
2026-02-19 00:33:07 +0000

    add tests

---
+++ src/test/java/uk/bit1/spring_jpa/entity/NormalisationRulesTest.java
@@ -0,0 +1,44 @@
+package uk.bit1.spring_jpa.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NormalisationRulesTest {
+
+    @Test
+    void tagNameIsStrippedAndLowercased() {
+        Tag t = new Tag("  UrGent  ");
+        assertThat(t.getName()).isEqualTo("urgent");
+    }
+
+    @Test
+    void customerNamesAreStripped() {
+        Customer c = new Customer("  Waters  ", "  Tony  ");
+        assertThat(c.getLastName()).isEqualTo("Waters");
+        assertThat(c.getFirstName()).isEqualTo("Tony");
+
+        c.changeName("  Tobes  ", "  Waters  ");
+        assertThat(c.getFirstName()).isEqualTo("Tobes");
+        assertThat(c.getLastName()).isEqualTo("Waters");
+    }
+
+    @Test
+    void ticketDescriptionIsStripped() {
+        Customer c = new Customer("Waters", "Tony");
+        Ticket t = c.raiseTicket("  This is a valid description.  ");
+
+        assertThat(t.getDescription()).isEqualTo("This is a valid description.");
+    }
+
+    @Test
+    void profileDisplayNameIsStripped() {
+        Customer c = new Customer("Waters", "Tony");
+        c.createProfile("  TonyW  ", false);
+
+        assertThat(c.getProfile().getDisplayName()).isEqualTo("TonyW");
+
+        c.getProfile().changeDisplayName("  NewName  ");
+        assertThat(c.getProfile().getDisplayName()).isEqualTo("NewName");
+    }
+}
